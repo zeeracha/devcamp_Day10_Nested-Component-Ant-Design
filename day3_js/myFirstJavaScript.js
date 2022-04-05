63d41e87601bb116a0d6ee4cc946d51df3969f76
Zeera <zeeraratcha@gmail.com>
2022-04-05 13:32:50 +0700

    practice debucking1

---
+++ day3_js/myFirstJavaScript.js
@@ -0,0 +1,13 @@
+debugger;
+let arr2 = [2,4,6,8,10];
+
+function myFunc(){
+    for (let i = 0; i < 2; i++) 
+    console.log('Hello World');
+}
+
+for (let i = 0; i < arr2.length; i++){
+    //debugger
+    console.log(arr2[i]);
+    myFunc();
+}
