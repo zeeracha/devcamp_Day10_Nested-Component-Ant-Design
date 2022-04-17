57214ff5f72643d5d1a18a32937d160557d183dd
Zeera <zeeraratcha@gmail.com>
2022-04-17 22:23:09 +0700

    edited

--- day3_jvs/3_3.js
+++ day3_jvs/3_3.js
@@ -1,24 +1,2 @@
 // 3.3
-let mylist =[];
-const draw = function(n){
-    for (let i = 0; i < n; j++){
-        let m = 1;
-        mylist.push([]);
-        for (let j = 0; j< n; j++){
-            if (m < i+1){
-                mylist[i].push("* ");
-            }else{
-                mylist[i].push("_ ");
-            }
-            m++;
-        }
-    }
-    for (let k = 0; k < mylist.length; k++){
-        let done = "";
-        for (let l =0; l < mylist[k].length; l++){
-            done += mylist[k][l];
-        }
-        console.log(done);
-    }
-}
-draw(4);
+//not finished yet
