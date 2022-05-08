200b0227469496482165c9d598084c2fd5cade01
zeeracha <102946057+zeeracha@users.noreply.github.com>
2022-05-08 21:52:10 +0700

    Delete Day9_React - Props, State, Events/lab directory

--- Day9_React - Props, State, Events/lab/lab1.js
+++
@@ -1,6 +0,0 @@
-class ProfileClassComp extends React.Component {
-    render() {
-        return (<><p>Firstname : {this.props.firstname}</p>
-            <p>Lastname : {this.props.lastname}</p></>)
-    }
-}
